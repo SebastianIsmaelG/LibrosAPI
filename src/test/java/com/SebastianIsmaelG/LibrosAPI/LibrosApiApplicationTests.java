8d506996438705c3896b8a6b62261372a084cca4
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2024-11-19 01:38:00 -0300

    Setup de spring

---
+++ src/test/java/com/SebastianIsmaelG/LibrosAPI/LibrosApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.SebastianIsmaelG.LibrosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("äsdasd");
+		//https://gutendex.com/?ref=public_apis
+	}
+
+}
